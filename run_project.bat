a04e45ccd97caa35e0b42e3c436b3afbda26557e
Eric Sun <ericsun987@gmail.com>
2025-10-16 23:41:36 -0700

    Moved frontend stuff to frontend/mobile directory.

--- run_project.bat
+++ run_project.bat
@@ -5,4 +5,4 @@ REM Start back-end in a new Command Prompt window
 start "Back-End" cmd /k "cd backend && npm start"
 
 REM Start front-end in a new Command Prompt window
-start "Front-End" cmd /k "flutter run --web-hostname=127.0.0.1 --web-port=8000"
+start "Front-End" cmd /k "cd frontend/mobile && flutter run --web-hostname=127.0.0.1 --web-port=8000"
